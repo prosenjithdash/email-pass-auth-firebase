2ba4f351d4fc7bd866daa33240b4a7e8f6aaec3e
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-09-14 01:10:58 +0600

    done Simple form, and daisy ui form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const Register = () => {
+
+
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log('Email: ',email ,'Password: ', password)
+    }
     return (
-        <div>
-            <h2 className="text2xl">Please Register</h2>
+        <div className='bg-orange-300 border-2 rounded-xl border-black-700 p-3 m-3 w-2/4 mx-auto'>
+            <h2 className="text-2xl text-center font-extrabold m-4">Please Register</h2>
+
+            <form onSubmit={handleRegister}>
+                <input type="email" placeholder='enter email' name="email" id="" className='border-2 w-3/4 border-red-600 mb-2 p-2' />
+                <br />
+                <input type="password" placeholder='enter password' name="password" id="" className='border-2 border-red-600 mb-2 p-2 w-3/4' />
+                <br />
+                <br />
+                <input type="submit" value="Register" className='border-2 bg-orange-600 text-white font-bold border-red-600 p-2 rounded-lg w-full mb-4' />
+            </form>
         </div>
     );
 };
